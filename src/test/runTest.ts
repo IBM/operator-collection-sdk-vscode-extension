813d6919b5395f9a1c83168cd2e50d25ef1d7038
Latrell Freeman <freemanlatrell@gmail.com>
2023-08-02 00:33:51 -0400

    Initial test updates

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,23 +1,46 @@
 import * as path from 'path';
+import * as fs from 'fs-extra';
+import * as os from 'os';
 
 import { runTests } from '@vscode/test-electron';
 
-async function main() {
+async function go() {
 	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
 		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
-
-		// The path to test runner
-		// Passed to --extensionTestsPath
 		const extensionTestsPath = path.resolve(__dirname, './suite/index');
+		const fixturePath = path.resolve(__dirname, '../../testFixures/zos_ims_operator');
+		const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'vscode-user'));
+		fs.copySync(
+			path.resolve(__dirname, '../../testFixures/vscode-user/User'),
+			path.join(tmpDir, 'User')
+		);
 
 		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
+		if (process.platform === 'win32') {
+			await runTests({
+				version: '1.80.0',
+				platform: 'win32-x64-archive',
+				extensionDevelopmentPath, 
+				extensionTestsPath,
+				launchArgs: [
+					fixturePath,
+					`--user-data-dir=${tmpDir}`
+				],
+			});
+		} else {
+			await runTests({ 
+				extensionDevelopmentPath, 
+				extensionTestsPath,
+				launchArgs: [
+					fixturePath,
+					`--user-data-dir=${tmpDir}`
+				],
+			});
+		}	
 	} catch (err) {
 		console.error('Failed to run tests', err);
 		process.exit(1);
 	}
 }
 
-main();
+go();
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,32 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as myExtension from '../../extension';
+import * as child_process from 'child_process';
+import {VSCodeCommands} from '../../utilities/commandConstants';
 
-suite('Extension Test Suite', () => {
+describe('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	before(async () => {
+		const extension = vscode.extensions.getExtension("ibm.operator-collection-sdk");
+  		await extension?.activate();
 	});
+
+	it('Should install the Operator Collection SDK', async () => {
+		await vscode.commands.executeCommand(VSCodeCommands.install);
+		try {
+			child_process.execSync("ansible-galaxy collection verify ibm.operator_collection_sdk");
+		} catch (e) {
+			assert.equal(e, undefined);
+		}
+	});
+
+	it('Create Operator', async () => {
+		try {
+			await vscode.commands.executeCommand(VSCodeCommands.createOperator);
+		} catch (e) {
+			assert.equal(e, undefined);
+		}
+	});
+	
 });
