bebfdbe28b26f4cc59b09ba038ed3b9ed60725f0
Latrell Freeman <freemanlatrell@gmail.com>
2023-08-22 16:51:05 -0400

    Debug

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -86,6 +86,7 @@ describe('Extension Test Suite', async () => {
 			// Update K8s object to retrieve config after log in
 			k8s = new helper.KubernetesObj();
 			userLoggedIn = await k8s.isUserLoggedIntoOCP();
+			console.log("User logged in Before: " + userLoggedIn);
 			assert.equal(userLoggedIn, true);
 
 			// Create Namespace if not already created
