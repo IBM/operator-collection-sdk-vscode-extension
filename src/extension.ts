3c97c0cc7352fea0d03fab017ca1beaa649f907f
Latrell Freeman <Latrell.Freeman@us.ibm.com>
2023-10-23 21:15:20 -0400

    Display warning when retrieving logs before tasks have executed

--- src/extension.ts
+++ src/extension.ts
@@ -608,8 +608,12 @@ function viewResourceCommand(
               group!,
               apiVersion!,
             );
-            const doc = await vscode.workspace.openTextDocument(uri);
-            await vscode.window.showTextDocument(doc, { preview: false });
+            try {
+              const doc = await vscode.workspace.openTextDocument(uri);
+              await vscode.window.showTextDocument(doc, { preview: false });
+            } catch (e) {
+              return;
+            }
           }
         })
         .catch((e) => {
@@ -689,15 +693,20 @@ function executeContainerViewLogCommand(
                             kind,
                             crInstance,
                           );
-                          const doc =
+                          try {
+                            const doc =
                             await vscode.workspace.openTextDocument(logUri);
-                          await vscode.window.showTextDocument(doc, {
-                            preview: false,
-                          });
-                          vscode.commands.executeCommand(
-                            VSCodeCommands.refreshVerboseContainerLog,
-                            logUri,
-                          );
+                            await vscode.window.showTextDocument(doc, {
+                              preview: false,
+                            });
+                            vscode.commands.executeCommand(
+                              VSCodeCommands.refreshVerboseContainerLog,
+                              logUri,
+                            );
+                          } catch (e) {
+                            return;
+                          }
+                          
                         }
                       }
                     }
